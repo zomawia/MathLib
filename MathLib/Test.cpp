4cced3e93577ef2c19927883407129f1f6cce622
zomawai.sailo <zomawai.sailo@SEA-8E40F1>
2016-09-26 10:59:26 -0700

    1st

---
+++ MathLib/Test.cpp
@@ -0,0 +1,4 @@
+int doNothing(int x)
+{
+	return x*x + 2 * x + 5;
+}
---
+++ MathTests/main.cpp
@@ -0,0 +1,13 @@
+#include <cassert>
+#include <cstdio>
+#include "Test.h"
+
+int main()
+{
+	printf("Hello World! %d", doNothing(6));
+	
+
+	getchar();
+
+	return 0;
+}
